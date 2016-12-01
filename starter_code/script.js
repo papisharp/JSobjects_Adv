91649767a0e202bf6b5f9abc09a677187deb4df3
papisharp <kevin.cordero@uasdc.org>
2016-12-01 23:19:26 +0000

    my comment

--- starter_code/script.js
+++ starter_code/script.js
@@ -1,13 +1,12 @@
 //Task 1: Console the year in which the song "Beat it" was relased 
-
-
+console.log(library.music.eighties.Michael_Jackson[0])
 //Task 2: Console the name of the song Lady Gaga released in 2006 
+console.log(library.music.two_thousands.Lady_Gaga[0])
 
 
 
-$( document ).ready(function() {
 //Task 3: Have the words "Rolling in the Deep" appear on the HTML page in a div tag.    
-    
+    $( document ).ready(function() {
 //Task 4: (project) Create a site that can display songs from an artist specified by a user search.
 
 });
